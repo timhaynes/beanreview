2966afc89276ad0a3e1fac4b1fd2a6916139129d
Tim Haynes <timhaynes@gmail.com>
2016-11-20 17:33:02 +1100

    AddBeanViewController * Country text field is now linked to tableView which suggests country from list of coffee producers * Added extension to String to allow capitalising of first letter of user input in country text field

--- BeanReview/AddBeanViewController.swift
+++ BeanReview/AddBeanViewController.swift
@@ -9,23 +9,79 @@
 import UIKit
 import CloudKit
 
-class AddBeanViewController: UIViewController {
+class AddBeanViewController: UIViewController, UITextFieldDelegate, UITableViewDelegate, UITableViewDataSource {
 
-    @IBOutlet var countryTextField: UITextField!
+    @IBOutlet weak var countryTextField: UITextField!
     @IBOutlet var producerTextField: UITextField!
     @IBOutlet var nameTextField: UITextField!
+    @IBOutlet weak var tableView: UITableView!
+    // List of coffee producing countries from Wikipedia
+    let beanCountries = ["Brazil", "Vietnam", "Colombia", "Indonesia", "Ethiopia", "India", "Mexico", "Guatemala", "Peru", "Honduras",
+                         "Uganda", "Ivory Coast", "China", "Costa Rica", "El Salvador", "Nicaragua", "Papua New Guinea", "Ecuador",
+                         "Thailand", "Tanzania", "Dominican Republic", "Kenya", "Venezuela", "Cameroon", "Philippines", "Democratic Republic of the Congo", "Burundi", "Madagascar", "Yemen", "Haiti", "Rwanda", "Guinea", "Cuba", "Togo",
+                         "Bolivia", "Zambia", "Angola", "Central African Republic", "Panama", "Zimbabwe", "United States", "Nigeria",
+                         "Ghana", "Jamaica", "Sri Lanka", "Malawi", "Paraguay", "Sierra Leone", "Australia", "Trinidad and Tobago", "Nepal", "Republic of the Congo", "Equatorial Guinea", "Gabon", "Benin"]
+    var autoComplete = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        countryTextField.delegate = self
+        tableView.delegate = self
+        
         let saveButton = UIBarButtonItem(barButtonSystemItem: .save, target: self, action: #selector(getUserInput))
         self.navigationItem.rightBarButtonItem = saveButton
-        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: Country textfield delegate
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let substring = (countryTextField.text! as NSString).replacingCharacters(in: range, with: string)
+        
+        if substring.characters.first == substring.lowercased().characters.first {
+            searchAutoCompleteEntriesWithSubstring(substring: substring.capitalizingFirstLetter())
+        } else {
+            searchAutoCompleteEntriesWithSubstring(substring: substring)
+        }
+        
+        return true
+    }
+    
+    func searchAutoCompleteEntriesWithSubstring(substring: String) {
+        autoComplete.removeAll(keepingCapacity: false)
+        
+        for key in beanCountries {
+            let myString:NSString! = key as NSString
+            let substringRange: NSRange! = myString.range(of: substring)
+            if (substringRange.location == 0) {
+                autoComplete.append(key)
+            }
+        }
+        
+        tableView.reloadData()
+    }
+    
+    // MARK: TableView
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as UITableViewCell
+        let index = indexPath.row as Int
+        cell.textLabel!.text = autoComplete[index]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return autoComplete.count
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let selectedCell: UITableViewCell = tableView.cellForRow(at: indexPath)!
+        countryTextField.text = selectedCell.textLabel!.text
     }
     
     func getUserInput() {
@@ -42,10 +98,9 @@ class AddBeanViewController: UIViewController {
     }
     
     func validateBean(country: String, producer: String, name: String) -> Bool {
-        if country == "" {
+        if country == "" || !beanCountries.contains(country) {
             showValidationFailAlert(message: "Please enter a valid country.")
             return false
-            // TODO validate against country list
         }
         if producer == "" {
             showValidationFailAlert(message: "Please enter a producer.")
@@ -94,4 +149,16 @@ class AddBeanViewController: UIViewController {
     }
     */
 
+}
+
+extension String {
+    func capitalizingFirstLetter() -> String {
+        let first = String(characters.prefix(1)).capitalized
+        let other = String(characters.dropFirst())
+        return first + other
+    }
+    
+    mutating func capitalizeFirstLetter() {
+        self = self.capitalizingFirstLetter()
+    }
 }
